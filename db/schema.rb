b7ddf3daa7ce4d800d96595a4c61c5167a555883
Ubuntu <ubuntu@ip-172-31-13-226.ec2.internal>
2020-11-25 12:51:04 +0000

    Add review book

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_25_082008) do
+ActiveRecord::Schema.define(version: 2020_11_25_082942) do
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -32,12 +33,20 @@ ActiveRecord::Schema.define(version: 2020_11_25_082008) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "book_reviews", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
+    t.text "review"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.integer "number_of_pages"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.text "detail"  
+    t.text "detail"
   end
 
   create_table "users", force: :cascade do |t|
@@ -53,7 +62,7 @@ ActiveRecord::Schema.define(version: 2020_11_25_082008) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    
   end
-add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
